030d8ab3e6d8de5e66d7b21790bc3f64bb7aa179
Tamiahill79 <123573883+Tamiahill79@users.noreply.github.com>
2025-07-01 04:53:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function showMessage() {
+  alert("Fun Fact: Blue Bell first released Strawberry in 1969!");
+}
+function RecipeMessage1() {
+  alert("You'll need 2 grahm crackers and strawberry ice cream. Put a scoop of ice cream on 1 grahm cracker and put the other grahm cracker on top of it. ENJOY!");
+}
+function RecipeMessage2() {
+  alert("You'll need strawberry icecream, ice cubes, milk, whipped cream, and a blender. Combine 2 scoops of ice cream, 1/2 cup of milk, and 5 ice cubes in a blender and blend. Once your ingredients are blended pour the mixture into a cup and top with whipped cream and any toppings of your choice. ENJOY! :)");
+}
+function RecipeMessage3() {
+  alert("You'll need strawberry icecream, strawberry soda, and whipped cream. Combine scoops of strawberry ice cream with any strawberry soda of your choice. Then top it with whipped cream and ENJOY! <3");
+}
